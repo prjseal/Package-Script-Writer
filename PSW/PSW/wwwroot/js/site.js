b44bd4cac1df1f18f1b246f1e11e2b2b6d62f7d5
Paul Seal <paul.seal@theaccessgroup.com>
2022-05-19 20:03:10 +0100

    finished ajaxify

--- PSW/PSW/wwwroot/js/site.js
+++ PSW/PSW/wwwroot/js/site.js
@@ -20,7 +20,8 @@
     buttons: {
         clearpackages: document.getElementById('clearpackages'),
         reset: document.getElementById('reset'),
-        copy: document.getElementById('copy')
+        copy: document.getElementById('copy'),
+        generate: document.getElementById('generate')
     },
     init: function () {
         psw.addListeners();
@@ -29,11 +30,13 @@
         psw.buttons.clearpackages.addEventListener('click', function (event) {
             psw.clearAllPackages(event);
             psw.updateOutput();
+            psw.updateUrl();
         });
 
         psw.buttons.reset.addEventListener('click', function (event) {
             psw.reset(event);
             psw.updateOutput();
+            psw.updateUrl();
         });
 
         psw.controls.search.addEventListener('keyup', psw.filterPackages);
@@ -41,44 +44,116 @@
         psw.controls.useUnattendedInstall.addEventListener('change', function () {
             psw.toggleUnattendedInstallControls();
             psw.updateOutput();
+            psw.updateUrl();
         });
 
         psw.controls.installUmbracoTemplate.addEventListener('change', function () {
             psw.toggleInstallUmbracoTemplateControls();
             psw.updateOutput();
+            psw.updateUrl();
         });
 
         psw.controls.includeStarterKit.addEventListener('change', function () {
             psw.toggleIncludeStarterKitControls();
             psw.updateOutput();
+            psw.updateUrl();
         });
 
         psw.controls.createSolutionFile.addEventListener('change', function () {
             psw.toggleCreateSolutionFileControls();
             psw.updateOutput();
+            psw.updateUrl();
         });
 
         psw.buttons.copy.addEventListener('click', function (event) {
             psw.copyCodeBlock(event);
-        })
+        });
+
+        psw.buttons.generate.addEventListener('click', function (event) {
+            event.preventDefault();
+            $('#code-tab').tab('show');
+            psw.updateOutput();
+            psw.updateUrl();
+        });
 
         psw.controls.packageCheckboxes.forEach(function (checkbox) {
             checkbox.addEventListener('change', function () {
                 psw.updatePackages(this.id);
                 psw.updateOutput();
+                psw.updateUrl();
             });
         });
 
         var tab = document.querySelector('#myTab a');
         tab.addEventListener('click', function (event) {
-            event.preventDefault()
-            $(this).tab('show')
+            event.preventDefault();
+            $(this).tab('show');
         });
 
         psw.controls.search.addEventListener('keypress', function () {
             if (event.key === "Enter") {
                 event.preventDefault();
             }
+        });
+
+        psw.controls.solutionName.addEventListener('keyup', psw.debounce(function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        }, 250));
+
+        psw.controls.projectName.addEventListener('keyup', psw.debounce(function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        }, 250));
+
+        psw.controls.userFriendlyName.addEventListener('keyup', psw.debounce(function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        }, 250));
+
+        psw.controls.userEmail.addEventListener('keyup', psw.debounce(function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        }, 250));
+
+        psw.controls.userPassword.addEventListener('keyup', psw.debounce(function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        }, 250));
+
+        psw.controls.installUmbracoTemplate.addEventListener('change', function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        });
+
+        psw.controls.includeStarterKit.addEventListener('change', function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        });
+
+        psw.controls.createSolutionFile.addEventListener('change', function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        });
+
+        psw.controls.useUnattendedInstall.addEventListener('change', function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        });
+
+        psw.controls.umbracoTemplateVersion.addEventListener('change', function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        });
+
+        psw.controls.starterKitPackage.addEventListener('change', function () {
+            psw.updateOutput();
+            psw.updateUrl();
+        });
+
+        psw.controls.databaseType.addEventListener('change', function () {
+            psw.updateOutput();
+            psw.updateUrl();
         });
     },
     updatePackages: function (checkboxId) {
@@ -223,7 +298,7 @@
             "StarterKitPackage": psw.controls.starterKitPackage.value
         }
 
-        var url = "https://localhost:7192/api/scriptgeneratorapi/generatescript";
+        var url = "/api/scriptgeneratorapi/generatescript";
 
         fetch(url, {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -234,18 +309,51 @@
             body: JSON.stringify(data) // body data type must match "Content-Type" header
         }).then((response) => {
             var result = response.text();
-            console.log(result);
             if (response.ok) {
                 return result;
             }
         }).then((data) => {
-            console.log(data);
             psw.controls.codeBlock.innerHTML = data;
             psw.controls.codeBlock.classList.remove('prettyprinted');
             PR.prettyPrint();
         }).catch((error) => {
             console.log(error);
         });
+    },
+    updateUrl: function () {
+        if ('URLSearchParams' in window) {
+            var searchParams = new URLSearchParams(window.location.search);
+            searchParams.set("InstallUmbracoTemplate", psw.controls.installUmbracoTemplate.checked);
+            searchParams.set("UmbracoTemplateVersion", psw.controls.umbracoTemplateVersion.value);
+            searchParams.set("Packages", psw.controls.packages.value);
+            searchParams.set("UserEmail", psw.controls.userEmail.value);
+            searchParams.set("ProjectName", psw.controls.projectName.value);
+            searchParams.set("CreateSolutionFile", psw.controls.createSolutionFile.checked);
+            searchParams.set("SolutionName", psw.controls.solutionName.value);
+            searchParams.set("UseUnattendedInstall", psw.controls.useUnattendedInstall.checked);
+            searchParams.set("DatabaseType", psw.controls.databaseType.value);
+            searchParams.set("UserPassword", psw.controls.userPassword.value);
+            searchParams.set("UmbracoTemplateVersion", psw.controls.umbracoTemplateVersion.value);
+            searchParams.set("UserFriendlyName", psw.controls.userFriendlyName.value);
+            searchParams.set("IncludeStarterKit", psw.controls.includeStarterKit.checked);
+            searchParams.set("StarterKitPackage", psw.controls.starterKitPackage.value);
+            var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
+            history.pushState(null, '', newRelativePathQuery);
+        }
+    },
+    debounce: function(func, wait, immediate) {
+        var timeout;
+        return function () {
+            var context = this, args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
     }
 }; psw.init();
 
