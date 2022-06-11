9a37141ded82e2b7f3f554ea4e38e29ebfee79a9
Aaron Sadler <aaron@aaronsadler.co.uk>
2022-06-11 13:26:35 +0100

    Doesn't look like this is nullable

--- PSW/PSW/Models/NuGet/PackageVersions.cs
+++ PSW/PSW/Models/NuGet/PackageVersions.cs
@@ -4,6 +4,11 @@ namespace PSW.Models.NuGet;
 
 public class PackageVersions
 {
+    public PackageVersions(string[] versions)
+    {
+        Versions = versions;
+    }
+
     [JsonPropertyName("versions")]
-    public string[]? Versions { get; set; }
+    public string[] Versions { get; set; }
 }
