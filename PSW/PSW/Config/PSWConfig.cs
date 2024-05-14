9e4c1379cf333ea7233443c61b1b4d810834cf6b
Paul Seal <prjseal@gmail.com>
2024-05-14 15:40:30 +0100

    added umbraco versions tab

---
+++ PSW/PSW/Config/PSWConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace PSW.Configuration
+{
+    public class PSWConfig
+    {
+        public static string SectionName => "PSW";
+        public List<UmbracoVersion> UmbracoVersions { get; set; }
+    }
+
+    public class UmbracoVersion
+    {
+        public int Version { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string? ReleaseType { get; set; }
+        public DateTime? SupportPhase { get; set; }
+        public DateTime? SecurityPhase { get; set; }
+        public DateTime EndOfLife { get; set; }
+    }
+}
--- PSW/PSW/Program.cs
+++ PSW/PSW/Program.cs
@@ -1,4 +1,5 @@
-﻿using PSW.Middleware;
+﻿using PSW.Configuration;
+using PSW.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,9 @@ builder.Services.AddHttpClient();
 builder.Services.AddScoped<IScriptGeneratorService, ScriptGeneratorService>();
 builder.Services.AddScoped<IPackageService, MarketplacePackageService>();
 builder.Services.AddScoped<IQueryStringService, QueryStringService>();
+
+builder.Services.Configure<PSWConfig>(
+    builder.Configuration.GetSection(PSWConfig.SectionName));
 
 var app = builder.Build();
 
