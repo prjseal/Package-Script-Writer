3362ca964a0c4bb7d07c3dab5f36c513d5be242e
Paul Seal <prjseal@gmail.com>
2024-05-16 10:36:34 +0100

    added emojis next to version number

--- PSW/PSW/Extensions/StringExtensions.cs
+++ PSW/PSW/Extensions/StringExtensions.cs
@@ -1,3 +1,7 @@
+﻿using System.Reflection;
+
+using PSW.Configuration;
+
 namespace PSW.Extensions;
 
 public static class StringExtensions
@@ -9,5 +13,52 @@ public static class StringExtensions
         if (value.Length > maxLength) { return value.Substring(0, maxLength) + "..."; }
 
         return value.Substring(0, value.Length);
+    }
+
+    public static string GetVersionText(this string version, string templateName, PSWConfig pswConfig)
+    {
+        if (string.IsNullOrWhiteSpace(version)) { return version; }
+
+        if (templateName != "Umbraco.Templates") { return version; }
+
+        var majorVersionNumberAsString = version.Split('.').FirstOrDefault();
+
+        if (string.IsNullOrWhiteSpace(majorVersionNumberAsString)) { return version; }
+
+        _ = int.TryParse(majorVersionNumberAsString, out var majorVersionNumber);
+
+        if (pswConfig.UmbracoVersions == null || !pswConfig.UmbracoVersions.Any()) return version;
+
+        if (!pswConfig.UmbracoVersions.Select(x => x.Version).Contains(majorVersionNumber)) { return version; }
+
+        var versionInUse = pswConfig.UmbracoVersions.FirstOrDefault(x => x.Version == majorVersionNumber);
+
+        var oneYearFromNow = DateTime.UtcNow.AddYears(1);
+        var isLTS = versionInUse.ReleaseType == "LTS";
+        var isEndOfLife = DateTime.UtcNow >= versionInUse.EndOfLife;
+        var isSTS = !isLTS;
+        var willEOLInLessThanAYear = !isEndOfLife && oneYearFromNow > versionInUse.EndOfLife;
+        var isFutureRelease = versionInUse.ReleaseDate > DateTime.UtcNow;
+
+        var suffix = "";
+
+        if (isEndOfLife)
+        {
+            suffix = "💀";
+        }
+        else if ((isSTS && !isFutureRelease) || willEOLInLessThanAYear)
+        {
+            suffix = "⚠️";
+        }
+        else if (isFutureRelease)
+        {
+            suffix = "🔮";
+        }
+        else
+        {
+            suffix = "✅";
+        }
+
+        return version + " " + suffix;
     }
 }
