b7f6d87ab488532f8b9724150eb69b48f515a67e
Paul Seal <prjseal@gmail.com>
2025-08-05 15:00:03 +0100

    added uui sandbox

---
+++ PSW/PSW/Controllers/UUIController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PSW.Controllers;
+public class UUIController : Controller
+{
+    public UUIController()
+    {
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
